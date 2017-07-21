860af884135a51431b6e454667a66e831a7a9890
salarhajimirsadeghi <salarhajimirsadeghi@gmail.com>
2017-07-21 11:46:52 -0700

    Finished script for unzipping files

--- unzipping_script.py
+++ unzipping_script.py
@@ -1,29 +1,32 @@
+"""
+This script 
+"""
+
+
 import os
 
 directory = "/Users/Salar_Hajimirsadeghi/Google Drive/Projects/Patents/Data"
 
 
 for folder in os.listdir(directory):	
-	if (folder.startswith('l')):	
-		# print ("Folder", folder)	
+	if (folder.startswith('l')):			
 
 		# Directory: ".../Data/l20170131/"
-		directory2 = directory +"/" +folder
-		# print("Directory:", directory2)
+		directory2 = directory +"/" +folder		
 		
 		for folder2 in os.listdir(directory2):			
+
 			# Directory: ".../Data/l20170131/I20170131"
 			if (folder2[1:] == folder[1:]):
-				directory3 = directory2 + "/" + folder2
-				# print ("New Directory:", directory3)
+				directory3 = directory2 + "/" + folder2			
 
 				for folder3 in os.listdir(directory3):
+
+					# Directory: ".../Data/l20170131/I20170131/UTIL____"
 					if(folder3.startswith('UTIL')):
-						last_directory = directory3 + "/" + folder3
-						print(last_directory)
+						last_directory = directory3 + "/" + folder3						
+						for f in os.listdir(last_directory):
+							os.system('unzip "*.zip"')
 
 
-
-
-# os.system('unzip "*.zip"')
 
